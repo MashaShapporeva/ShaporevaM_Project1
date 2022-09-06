9eabc8828c94026908e407619cc7760a6d1cd896
MashaShapporeva <92892250+MashaShapporeva@users.noreply.github.com>
2022-09-06 19:14:12 -0400

    Update Program.cs

--- ShaporevaM_Project1/Program.cs
+++ ShaporevaM_Project1/Program.cs
@@ -1,20 +1,40 @@
-﻿
 namespace helloworld_2_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-
-            string stg = "Hello, World! (2)";
-            Console.WriteLine(stg);
-
-            Console.WriteLine("enter username");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Enter user's age");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Write is: " + userName + " and the age is: " + age);
+            Console.WriteLine("What grade do you expect to get?");
+            int grade = Int32.Parse(Console.ReadLine());
+            string gradeLetter = "";
+            if (grade >= 98)
+                gradeLetter = "A+";
+            if (grade >= 92 && grade <= 97)
+                gradeLetter = "A";
+            if (grade >= 90 && grade <= 91)
+                gradeLetter = "A-";
+            if (grade >= 88 && grade <= 89)
+                gradeLetter = "B+";
+            if (grade >= 82 && grade <= 87)
+                gradeLetter = "B";
+            if (grade >= 80 && grade <= 81)
+                gradeLetter = "B-";
+            if (grade >= 78 && grade <= 79)
+                gradeLetter = "C+";
+            if (grade >= 72 && grade <= 77)
+                gradeLetter = "C";
+            if (grade >= 70 && grade <= 71)
+                gradeLetter = "C-";
+            if (grade >= 68 && grade <= 69)
+                gradeLetter = "D+";
+            if (grade >= 62 && grade <= 67)
+                gradeLetter = "D";
+            if (grade >= 60 && grade <= 61)
+                gradeLetter = "D-";
+            if (grade < 60)
+                gradeLetter = "F";
+            Console.WriteLine(gradeLetter);
+            
         }
     }
 }
